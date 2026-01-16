0ed8f1aa5cad136117498895f8afbb45378b3490
mitsu141 <cocoroggg@gmail.com>
2026-01-16 18:49:45 +0900

    Update gas.js

--- gas.js
+++ gas.js
@@ -6,10 +6,10 @@ const gasStations = [
     mapUrl: "https://maps.app.goo.gl/afWYUNoPESWSJQcT8"
   },
   {
-    name: "白馬村SS",
-    price: "レギュラー 172円/L",
-    embedUrl: "ここにGoogleMap埋め込みURL",
-    mapUrl: "ここに通常のGoogleMap URL"
+    name: "(有)あずさ石油",
+    price: "レギュラー 150円/L",
+    embedUrl: "<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3453.622608697165!2d137.93189597592968!3d36.31209399506178!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x601d72b0817cff6b%3A0x3e720c5387616e19!2z5pyJ6ZmQ5Lya56S-44GC44Ga44GV55-z5rK5!5e1!3m2!1sja!2sjp!4v1768556969341!5m2!1sja!2sjp" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>",
+    mapUrl: "https://maps.app.goo.gl/CSzWWqjY1nW4rfr59"
   }
 ];
 
